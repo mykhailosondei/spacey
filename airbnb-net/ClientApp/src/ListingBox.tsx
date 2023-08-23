114d7b1d2227729897ba84fc4cec9e4dbf405356
Mykhailo Sondei <mykhailosondei@gmail.com>
2023-08-22 22:51:34 -0500

    generated bullets and their visibility logic, refactored slidingTween method

--- airbnb-net/ClientApp/src/ListingBox.tsx
+++ airbnb-net/ClientApp/src/ListingBox.tsx
@@ -1,4 +1,4 @@
-import React, {MouseEventHandler, useRef, useState} from 'react';
+import React, {MouseEventHandler, useEffect, useRef, useState} from 'react';
 import './ListingBox.scss';
 import {ReactComponent as Like} from './like.svg'
 import Arrow from "./Arrow";
@@ -32,25 +32,9 @@ const ListingBox: React.FC<ListingBoxProps> = ({
     const [isLeftArrowVisible, setIsLeftArrowVisible] = useState(false);
     const [isRightArrowVisible, setIsRightArrowVisible] = useState(false);
     const pictureContainer = useRef<HTMLDivElement>(null);
+    const bulletsRef = useRef<HTMLDivElement>(null);
+    const bulletsArrayInitialState : {id:number, scale:number, opacity:0.6|1}[]= [];
     
-    function OnListingBoxMouseEnter(){
-        switch (currentPictureIndex){
-            case 0: 
-                setIsRightArrowVisible(true);
-                break;
-            case pictures.length-1:
-                setIsLeftArrowVisible(true);
-                break;
-            default:
-                setIsRightArrowVisible(true);
-                setIsLeftArrowVisible(true);
-        }
-    }
-
-    function OnListingBoxMouseLeave(){
-        setIsRightArrowVisible(false);
-        setIsLeftArrowVisible(false);
-    }
     
     for(let i: number = 0; i<pictures.length; i++){
         pictureArray.push(<a className="image-ref">
@@ -60,6 +44,90 @@ const ListingBox: React.FC<ListingBoxProps> = ({
                 </div>
             </div>
         </a>)
+        bulletsArrayInitialState.push({id:i, scale: 2/3, opacity: 0.6});
+    }
+    
+    const [bulletsArrayState, setBulletsArrayState] = useState<{id:number, scale:number, opacity:0.6|1}[]>(bulletsArrayInitialState);
+    
+    const [htmlBulletsArrayState, setHtmlBulletsArrayState] = useState<JSX.Element[]>(()=>{
+        const htmlBulletsArrayInitValue = [];
+        for (let i: number = 0; i<pictures.length; i++){
+            htmlBulletsArrayInitValue.push(
+                <span className={"bullet"}></span>
+            )
+        }
+        
+        return htmlBulletsArrayInitValue;
+    });
+    
+    function renderBullets(){
+        const htmlBulletsArrayValue = [];
+        for (let i: number = 0; i<pictures.length; i++){
+            htmlBulletsArrayValue.push(
+                <span className={"bullet" +" "+ ((bulletsArrayState[i].opacity == 1) ? "opacity-high" : "opacity-low") + " " + ((bulletsArrayState[i].scale == 1)? "scale-high":((bulletsArrayState[i].scale == 5/6)? "scale-mid":"scale-low"))}></span>
+            )
+        }
+        setHtmlBulletsArrayState(htmlBulletsArrayValue);
+    }
+    
+    function ValidateBulletsViewDependingOnIndex(){
+        const currentBulletHighlightedArrayState : {id:number, scale:number, opacity:0.6|1}[] = bulletsArrayState.map(bullet=>{
+            if(currentPictureIndex==bullet.id){
+                return {id:bullet.id, scale:bullet.scale, opacity:1};
+            }
+            else{
+                return {id:bullet.id, scale:bullet.scale, opacity:0.6};
+            }
+        });
+        const currentBulletScaleArrayState : {id:number, scale:number, opacity:0.6|1}[] = currentBulletHighlightedArrayState.map(bullet=>{
+            if(currentPictureIndex<=2){
+                if(bullet.id<=2) return {id:bullet.id, scale:1, opacity:bullet.opacity};
+                if(bullet.id==3) return {id:bullet.id, scale:5/6, opacity:bullet.opacity};
+                return {id:bullet.id, scale:2/3, opacity:bullet.opacity};
+            }
+            if(currentPictureIndex>=pictures.length-3){
+                if(bullet.id>=pictures.length-3) return {id:bullet.id, scale:1, opacity:bullet.opacity};
+                if(bullet.id==pictures.length-4) return {id:bullet.id, scale:5/6, opacity:bullet.opacity};
+                return {id:bullet.id, scale:2/3, opacity:bullet.opacity};
+            }
+            if(bullet.id<=currentPictureIndex+1 && bullet.id>=currentPictureIndex-1) return {id:bullet.id, scale:1, opacity:bullet.opacity};
+            if(bullet.id==currentPictureIndex+2 || bullet.id==currentPictureIndex-2) return {id:bullet.id, scale:5/6, opacity:bullet.opacity};
+            return {id:bullet.id, scale:2/3, opacity:bullet.opacity};
+        });
+        console.log(JSON.stringify(currentBulletScaleArrayState));
+        setBulletsArrayState(currentBulletScaleArrayState);
+    }
+    
+    
+    
+    function ValidateArrowsVisibilityDependingOnIndex(){
+        switch (currentPictureIndex){
+            case 0: 
+                setIsRightArrowVisible(true);
+                setIsLeftArrowVisible(false);
+                break;
+            case pictures.length-1:
+                setIsLeftArrowVisible(true);
+                setIsRightArrowVisible(false);
+                break;
+            default:
+                setIsRightArrowVisible(true);
+                setIsLeftArrowVisible(true);
+        }
+    }
+
+    useEffect(() => {
+        renderBullets();
+    }, [bulletsArrayState]);
+    
+    useEffect(()=>{
+        ValidateArrowsVisibilityDependingOnIndex();
+        ValidateBulletsViewDependingOnIndex();
+    },[currentPictureIndex]);
+
+    function OnListingBoxMouseLeave(){
+        setIsRightArrowVisible(false);
+        setIsLeftArrowVisible(false);
     }
     
     const animate = (t: any = 0) => {
@@ -67,21 +135,50 @@ const ListingBox: React.FC<ListingBoxProps> = ({
         window.requestAnimationFrame(animate);
     };
     
+    
+    function leftArrowOnClickFunction(){
+        //movePreviousBullet();
+        movePreviousSlide();
+    }
+
+    function rightArrowOnClickFunction(){
+        //moveNextBullet();
+        moveNextSlide();
+    }
+
+    function moveNextBullet(){
+        if(currentPictureIndex >= pictures.length-1){
+            return;
+        }
+        animate();
+        const fromValue = {margin: -11*(currentPictureIndex)}
+        const toValue = {margin: -11*(currentPictureIndex+1)}
+        const tween = slidingTween(fromValue, toValue, Easing.Cubic.Out, bulletsRef);
+        setCurrentPictureIndex((currentPictureIndex)=>currentPictureIndex+1);
+        tween.start();
+    }
+    
+    function movePreviousBullet(){
+        if(currentPictureIndex >= pictures.length-1){
+            return;
+        }
+        animate();
+        const fromValue = {margin: -11*(currentPictureIndex)}
+        const toValue = {margin: -11*(currentPictureIndex-1)}
+        const tween = slidingTween(fromValue, toValue, Easing.Cubic.Out, bulletsRef);
+        setCurrentPictureIndex((currentPictureIndex)=>currentPictureIndex+1);
+        tween.start();
+    }
+    
     function moveNextSlide(){
         if(currentPictureIndex >= pictures.length-1){
             return;
         }
         animate();
-        
-        const value = {margin: -100*(currentPictureIndex+1)}
-        const tween = new Tween({margin: -100*currentPictureIndex}).to(value, 300)
-            .onUpdate((value)=>{
-                if(pictureContainer.current){
-                    pictureContainer.current.style.transform = `translate(${value.margin}%)`
-                }
-            })
-            .easing(Easing.Cubic.Out);
-        setCurrentPictureIndex(currentPictureIndex+1);
+        const fromValue = {margin: -100*(currentPictureIndex)}
+        const toValue = {margin: -100*(currentPictureIndex+1)}
+        const tween = slidingTween(fromValue, toValue, Easing.Cubic.Out, pictureContainer)
+        setCurrentPictureIndex((currentPictureIndex)=>currentPictureIndex+1);
         tween.start();
     }
     
@@ -90,25 +187,32 @@ const ListingBox: React.FC<ListingBoxProps> = ({
             return;
         }
         animate();
-
-        const value = {margin: -100*(currentPictureIndex-1)}
-        const tween = new Tween({margin: -100*(currentPictureIndex)}).to(value, 300)
-            .onUpdate((value)=>{
-                if(pictureContainer.current){
-                    pictureContainer.current.style.transform = `translate(${value.margin}%)`
-                }
-            })
-            .easing(Easing.Cubic.Out);
-        setCurrentPictureIndex(currentPictureIndex-1);
+        const fromValue = {margin: -100*(currentPictureIndex)}
+        const toValue = {margin: -100*(currentPictureIndex-1)}
+        const tween = slidingTween(fromValue,toValue, Easing.Cubic.Out , pictureContainer)
+        setCurrentPictureIndex((currentPictureIndex)=>currentPictureIndex-1);
         tween.start();
     }
+    
+    
+    
+    function slidingTween(fromValue:{margin:number}, toValue : {margin:number}, easing:any, ref:React.RefObject<HTMLDivElement>){
+        return new Tween(fromValue).to(toValue, 300)
+            .onUpdate((value)=>{
+                if(ref.current){
+                    ref.current.style.transform = `translate(${value.margin}%)`
+                }
+            })
+            .easing(easing);
+    }
+    
     
     function changeLikeStatus(){
         setLiked(!liked);
     }
 
     return (
-        <div className="listing-box" onMouseEnter={OnListingBoxMouseEnter} onMouseLeave={OnListingBoxMouseLeave}>
+        <div className="listing-box" onMouseEnter={ValidateArrowsVisibilityDependingOnIndex} onMouseLeave={OnListingBoxMouseLeave}>
             <div className="picture-slider">
                 <div className="pictures-controls-and-window">
                     <div className="picture-controls">
@@ -123,13 +227,25 @@ const ListingBox: React.FC<ListingBoxProps> = ({
                             </div>
                             <div className="arrows-grid">
                                 <div className="left-arrow">
-                                    <Arrow svg={<LeftArrow />} isVisibleInitial={isLeftArrowVisible} onClickFunction={movePreviousSlide}></Arrow>
+                                    <Arrow svg={<LeftArrow />} isVisibleInitial={isLeftArrowVisible} onClickFunction={leftArrowOnClickFunction}></Arrow>
                                 </div>
                                 <div className="right-arrow">
-                                    <Arrow svg={<RightArrow />} isVisibleInitial={isRightArrowVisible} onClickFunction={moveNextSlide}></Arrow>
+                                    <Arrow svg={<RightArrow />} isVisibleInitial={isRightArrowVisible} onClickFunction={rightArrowOnClickFunction}></Arrow>
                                 </div>
                             </div>
-                            <div className="bottom-points"></div>
+                            <div className="bottom-points">
+                                <div className="justify-self-start"></div>
+                                <div className="justify-self-center">
+                                    <div className="bullets-holder">
+                                        <div className="bullets-container">
+                                            <div className="bullets" ref={bulletsRef}>
+                                                {htmlBulletsArrayState}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="justify-self-end"></div>
+                            </div>
                         </div>
                     </div>
                     <div className="window">
