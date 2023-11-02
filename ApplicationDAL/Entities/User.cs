331ad9c7ff5e6571ca038e0a907b651990215306
mykhailosondei <mykhailosondei@gmail.com>
2023-11-01 22:49:07 -0500

    add user and host entities

---
+++ ApplicationDAL/Entities/User.cs
@@ -0,0 +1,30 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ApplicationDAL.Entities;
+
+public class User
+{
+    [BsonRepresentation(BsonType.ObjectId)]
+    public Guid Id { get; set; }
+    
+    public string Name { get; set; }
+    
+    public string Email { get; set; }
+    
+    public string PhoneNumber { get; set; }
+    
+    public DateTime CreatedAt { get; set; }
+    
+    public string Password { get; set; }
+    
+    public string Address { get; set; }
+    
+    public string Description { get; set; }
+    
+    public string ProfilePictureUrl { get; set; }
+    
+    public List<Booking> Bookings { get; set; }
+    
+    public List<Review> Reviews { get; set; }
+}
