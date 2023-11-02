4ad3c38b4c23b8a2ae8c90c8ba7d6f7c8ab1a3d2
mykhailosondei <mykhailosondei@gmail.com>
2023-11-01 22:50:08 -0500

    add booking query repo

---
+++ ApplicationDAL/DataQueryAccess/BookingQueryRepository.cs
@@ -0,0 +1,34 @@
+using ApplicationDAL.DataQueryAccess.Abstract;
+using ApplicationDAL.Entities;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace ApplicationDAL.DataQueryAccess;
+
+public class BookingQueryRepository : BaseQueryRepository
+{
+    private readonly IMongoCollection<Booking> _collection = GetCollection<Booking>("bookings");
+    
+    public async Task<Booking> GetBookingById(Guid id)
+    {
+        var filter = Builders<Booking>.Filter.Eq("Id", id);
+        return await _collection.Find(filter).FirstOrDefaultAsync();
+    }
+    
+    public async Task<IEnumerable<Booking>> GetAllBookings()
+    {
+        return await _collection.Find(new BsonDocument()).ToListAsync();
+    }
+    
+    public async Task<IEnumerable<Booking>> GetBookingsByUserId(Guid userId)
+    {
+        var filter = Builders<Booking>.Filter.Eq("UserId", userId);
+        return await _collection.Find(filter).ToListAsync();
+    }
+    
+    public async Task<IEnumerable<Booking>> GetBookingsByListingId(Guid listingId)
+    {
+        var filter = Builders<Booking>.Filter.Eq("ListingId", listingId);
+        return await _collection.Find(filter).ToListAsync();
+    }
+}
