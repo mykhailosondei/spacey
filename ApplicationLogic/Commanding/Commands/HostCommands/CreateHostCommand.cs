3526eb2d8c6a2ba485dd7b0b582485f33459acff
mykhailosondei <mykhailosondei@gmail.com>
2023-11-19 22:20:05 -0600

    delete separated host creation logic

--- ApplicationLogic/Commanding/Commands/HostCommands/CreateHostCommand.cs
+++
@@ -1,6 +0,0 @@
-using ApplicationCommon.DTOs.Host;
-using MediatR;
-
-namespace ApplicationLogic.Commanding.Commands.HostCommands;
-
-public record CreateHostCommand(HostCreateDTO Host) : IRequest<Guid>;
