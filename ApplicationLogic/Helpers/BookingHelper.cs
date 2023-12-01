ca6e99ebf45d01bf86297d4c8375d53b87a7bc4a
mykhailosondei <mykhailosondei@gmail.com>
2023-11-30 18:58:27 -0600

    add guards to calculateRating method

--- ApplicationLogic/Helpers/BookingHelper.cs
+++ ApplicationLogic/Helpers/BookingHelper.cs
@@ -10,7 +10,7 @@ public static class BookingHelper
         return checkIn < checkOut;
     }
     
-    public static bool DateIntersects(DateTime inputCheckIn, DateTime inputCheckOut, IEnumerable<(DateTime checkIn, DateTime checkOut)> existingBookings)
+    public static bool DateIntersects(DateTime inputCheckIn, DateTime inputCheckOut, (DateTime checkIn, DateTime checkOut)[] existingBookings)
     {
         return existingBookings.Any(existingBooking => inputCheckIn < existingBooking.checkOut && inputCheckOut > existingBooking.checkIn);
     }
@@ -27,6 +27,14 @@ public static class BookingHelper
 
     public static double[] CalculateNewRating(int bookingsIdsCount, double[] listingRatings, double[] reviewRatings)
     {
+        if(listingRatings == null || listingRatings.Length == 0)
+        {
+            return reviewRatings;
+        }
+        if(listingRatings.Length != reviewRatings.Length)
+        {
+            throw new ArgumentException("The number of ratings in the review must match the number of ratings in the listing.");
+        }
         var newRatings = new double[reviewRatings.Length];
         double [] totalRatings = new double[reviewRatings.Length];
         var totalNumberOfRatings = bookingsIdsCount + 1;
