2eaafe2319045eed0856385927c99cac31d24939
mykhailosondei <mykhailosondei@gmail.com>
2024-02-05 18:30:18 -0600

    add navdropdown-guests

--- airbnb-net/ClientApp/src/components/NavBarDropdownGuests.tsx
+++ airbnb-net/ClientApp/src/components/NavBarDropdownGuests.tsx
@@ -1,3 +1,18 @@
-export const NavBarDropdownGuests = () => {
-    return null;
+import React from "react";
+
+interface NavBarDropdownGuestsProps {
+    active: boolean;
+    guests: number;
+    setGuests: (guests: number) => void;
+}
+
+export const NavBarDropdownGuests = (props : NavBarDropdownGuestsProps) => {
+    return props.active? <div className={"nav-dropdown right-40 nav-guests"}>
+        Guests
+        <div className="guest-counter">
+            <button className="guest-counter-button" onClick={() => props.setGuests(props.guests-1)} disabled={props.guests==0}>-</button>
+            <div className="guest-counter-number">{props.guests}</div>
+            <button className="guest-counter-button" onClick={() => props.setGuests(props.guests+1)} disabled={props.guests==4}>+</button>
+        </div>
+    </div>: <></>;
 };
