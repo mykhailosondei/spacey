9cc4646727ef1f16daa6d4d9341dcdc662f5dd3d
mykhailosondei <mykhailosondei@gmail.com>
2024-02-08 00:07:26 -0600

    add proper date filter

--- ApplicationLogic/Filters/DateFilter.cs
+++ ApplicationLogic/Filters/DateFilter.cs
@@ -1,7 +1,10 @@
+using System.Globalization;
+using System.Text;
 using ApplicationDAL.Entities;
 using ApplicationLogic.Filters.Abstract;
 using MongoDB.Bson;
 using MongoDB.Driver;
+using Newtonsoft.Json;
 
 namespace ApplicationLogic.Filters;
 
@@ -16,23 +19,20 @@ public class DateFilter : AbstractFilter
         _checkOut = checkOut;
     }
 
-    interface IMongoCollectionMock<TDocument> : IMongoCollection<TDocument>
-    {
-        new CollectionNamespace CollectionNamespace { get; }
-    }
-
     public override PipelineDefinition<Listing, Listing> BuildDefinition()
     {
-
+        var @string =
+            $"{{ $match: {{ bookings: {{ $elemMatch: {{ $or: [ {{ CheckIn: {{ $gte: ISODate(\"{_checkOut.Date.ToShortDateString()}\") }} }}, {{ CheckOut: {{ $lte: ISODate(\"{_checkIn.Date.ToShortDateString()}\") }} }} ] }} }} }} }}";
+        
+        Console.WriteLine(@string);
+        
         var aggregationPipeline = new BsonDocument[]
         {
             BsonDocument.Parse("{ $lookup: { from: 'bookings', localField: 'BookingsIds', foreignField: '_id', as: 'bookings' } }"),
-            BsonDocument.Parse(string.Format(
-                "{ $match: { bookings: { $not: { $elemMatch: { $and: [ { CheckIn: { $lte: \"{checkIn}\" } }, { CheckOut: { $gte: \"{checkOut}\" } } ] } } } } }",
-                _checkIn,
-                _checkOut)),
+            BsonDocument.Parse(@string),
             BsonDocument.Parse("{ $project: { bookings: 0 } }")
         };
+
         
         var pipeline = PipelineDefinition<Listing, Listing>.Create(aggregationPipeline);
         
