ee69d8311918545977c0fddf5dba7fc10e9576ce
mykhailosondei <mykhailosondei@gmail.com>
2024-02-09 22:59:41 -0600

    refactor DateFilter

--- ApplicationLogic/Filters/DateFilter.cs
+++ ApplicationLogic/Filters/DateFilter.cs
@@ -10,20 +10,47 @@ using Newtonsoft.Json;
 
 namespace ApplicationLogic.Filters;
 
-public class DateFilter : AbstractFilter
+public sealed class DateFilter : AbstractFilter
 {
-    private readonly DateTime _checkIn;
-    private readonly DateTime _checkOut;
+    private readonly DateTime? _checkIn;
+    private readonly DateTime? _checkOut;
 
-    public DateFilter(DateTime checkIn, DateTime checkOut)
+    public DateFilter(DateTime? checkIn, DateTime? checkOut)
     {
         _checkIn = checkIn;
         _checkOut = checkOut;
     }
     
+    protected override bool IsEmpty()
+    {
+        return _checkIn == null && _checkOut == null;
+    }
+    
     public override async Task<List<ListingAndBookings>> ApplyFilter(List<ListingAndBookings> listings)
     {
+        if (IsEmpty())
+        {
+            return listings;
+        }
+
+        (DateTime start, DateTime end)? range = null;
+        
+        if (_checkIn == null)
+        {
+            range ??= (_checkOut.Value, _checkOut.Value);
+        }
+        
+        if (_checkOut == null)
+        {
+            range ??= (_checkIn.Value, _checkIn.Value);
+        }
+        
+        range ??= (_checkIn.Value, _checkOut.Value);
+        
+        listings.ForEach(listing => 
+            Console.WriteLine(listing.Bookings.Count())
+            );
         return listings.Where(listing =>
-            listing.Bookings.All(booking => booking.CheckOut < _checkIn || booking.CheckIn > _checkOut)).ToList();
+            listing.Bookings.All(booking => booking.CheckOut < range.Value.start || booking.CheckIn > range.Value.end)).ToList();
     }
 }
