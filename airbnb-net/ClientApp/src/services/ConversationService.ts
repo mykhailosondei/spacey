2bdf0b09983661c73dde14381897321a2616914d
mykhailosondei <mykhailosondei@gmail.com>
2024-02-28 00:13:19 -0600

    init front conv service

---
+++ airbnb-net/ClientApp/src/services/ConversationService.ts
@@ -0,0 +1,32 @@
+import {HttpCustomClient} from "./HttpCustomClient";
+
+export default class ConversationService {
+    http: HttpCustomClient;
+    baseUrl: string;
+    
+    private constructor() {
+        this.http = HttpCustomClient.getInstance();
+        this.baseUrl = "/conversation";
+    }
+    
+    private static instance: ConversationService;
+    
+    public static getInstance(): ConversationService {
+        if (!ConversationService.instance) {
+            ConversationService.instance = new ConversationService();
+        }
+        return ConversationService.instance;
+    }
+    
+    async get(id: string) {
+        return await this.http.Get(`${this.baseUrl}/${id}`);
+    }
+    
+    async sendMessage(conversationId: string, message: string) {
+        return await this.http.Post(`${this.baseUrl}/${conversationId}`, {message});
+    }
+    
+    async create() {
+        return await this.http.Post(`${this.baseUrl}`);
+    }
+}
